93556046f72ab74e317de76533bbb75fabe011fa
jjolly441 <33730216+jjolly441@users.noreply.github.com>
2022-07-04 14:32:11 -0400

    First commit

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Blog
+
+
+def all_blogs(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+# Create your views here.
+
+def detail(request, blog_id):
+    return render(request, 'blog/detail.html', {'id':blog_id})
